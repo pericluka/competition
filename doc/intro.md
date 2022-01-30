e4af9fce05738c4edc04698780e5884b9cc61c62
Luka Peric <lukap@saglobal.com>
2022-01-30 14:03:08 +0100

    First commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to competition
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/competition/core_test.clj
@@ -0,0 +1,7 @@
+(ns competition.core-test
+  (:require [clojure.test :refer :all]
+            [competition.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
